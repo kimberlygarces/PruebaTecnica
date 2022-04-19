5ad71eb0af65bf4511ec7a191fbb66d49c07cb59
kimberlygarces <kimberly.gp@hotmail.com>
2022-04-19 18:24:59 -0500

    first commit

--- productos.sql
+++ productos.sql
@@ -1,13 +1,4 @@
-
---
--- Database: `tabla`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `productos`
---
+CREATE DATABASE TABLAPRUEBA;
 
 CREATE TABLE `productos` (
   `ID` int(11) NOT NULL,
@@ -17,31 +8,18 @@ CREATE TABLE `productos` (
   `PRECIO` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `productos`
---
 
+ 
+
+-- En la tabla [Productos] anteriormente creada por favor haga la inserción de 3 registros que completen todas las columnas: 
 INSERT INTO `productos` (`ID`, `REFERENCIA`, `DESCRIPCION`, `STOCK`, `PRECIO`) VALUES
 (1, '12312D', 'LIBRETA', 5, 5000),
 (2, '2345', 'LAPIZ', 9, 500),
 (3, '12342', 'BORRADOR', 5, 6000);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `productos`
---
 ALTER TABLE `productos`
-  ADD PRIMARY KEY (`ID`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
-- AUTO_INCREMENT for table `productos`
+-- AUTO_INCREMENT for table `productos`
 
 ALTER TABLE `productos`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
@@ -49,14 +27,20 @@ COMMIT;
 
 
 
+-- esta se realizan los puntos especificos de la prueba
+
+ 
+
+-- Teniendo registros en nuestra tabla de [Productos] vamos a realizar 2 modificaciones, la modificación del precio en el segundo registro (producto) y la modificación del [STOCK] del primero por su [ID]: 
+
 UPDATE `productos` SET `PRECIO`='900' WHERE ID = 2;
 
 UPDATE `productos` SET `STOCK`='400' WHERE ID = 1;
 
-
+-- En nuestra tabla [Productos] vamos a realizar la eliminación del tercer registro usando su [ID]: 
 DELETE FROM `productos` WHERE ID = 3;
 
-
+-- Haga un conteo de los registros y una suma del [STOCK] de la tabla [Productos], tenga en cuenta que esto son 2 consultas: 
 select count(*) from productos;
 
 
